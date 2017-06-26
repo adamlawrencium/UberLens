170ba26cfe88b182780ca2a573ad9de359c1fb4a
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-06-25 19:46:08 -0700

    added plotting functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import googlemaps
+import gmplot
 from datetime import datetime
 
 
@@ -43,8 +44,10 @@ if __name__ == '__main__':
     msft_seattle = '320 Westlake Ave N, Seattle, WA 98109'
     msft_bellevue = '205 108th Ave NE, Bellevue, WA 98004'
     msft_redmond = '15010 Northeast 36th Street, Redmond, WA 98052'
+    larkspur = '15805 SE 37th St, Bellevue, WA 98006'
 
     # # # # #
+
     Uber = UberWrapper()
     APIKEY = readAPI_key()
     gmaps = googlemaps.Client(key=APIKEY)
@@ -57,8 +60,23 @@ if __name__ == '__main__':
 
     nearby_result = gmaps.places_radar(latlng, 5000, keyword='address')
     nearby_result = nearby_result['results']
+    lats = []
+    lngs = []
     for r in nearby_result:
         pair = r['geometry']['location']['lat'], r['geometry']['location']['lng']
         print 'Gmaps:\t', get_address_from_reverse_geocode(gmaps.reverse_geocode(pair))
         print 'Uber:\t', (Uber.address_lookup(pair[0], pair[1]))['longAddress']
         print
+        lats.append(pair[0])
+        lngs.append(pair[1])
+
+
+
+
+    gmap = gmplot.GoogleMapPlotter(latlng[0], latlng[1], 14)
+
+    # gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
+    gmap.scatter(lats, lngs, 'k', marker=True)
+    # gmap.heatmap(heat_lats, heat_lngs)
+
+    gmap.draw("mymap.html")
