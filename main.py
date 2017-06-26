a705a8c02a4f195bbe6b940386a7d7c4e4bbfe11
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-06-25 23:47:01 -0700

    added hex grid functionality

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import json
 import googlemaps
 import gmplot
+from hexmath import generate_hexgrid
+import params
 from datetime import datetime
 
 
@@ -21,6 +23,14 @@ def get_address_from_reverse_geocode(blob):
     address = blob[0]['formatted_address']
     return address
 
+def drawMap():
+    gmap = gmplot.GoogleMapPlotter(params.centroid[0], params.centroid[1], 14)
+
+    # gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
+    gmap.scatter(lats, lngs, 'k', marker=True)
+    # gmap.heatmap(heat_lats, heat_lngs)
+
+    gmap.draw("mymap.html")
 
 
 class UberWrapper(object):
@@ -52,31 +62,33 @@ if __name__ == '__main__':
     APIKEY = readAPI_key()
     gmaps = googlemaps.Client(key=APIKEY)
 
-    geocode_result = gmaps.geocode(msft_bellevue)
+    geocode_result = gmaps.geocode(msft_seattle)
     latlng = get_lat_lng_from_geocode(geocode_result)
+    print latlng
 
     reverse_geocode_result = gmaps.reverse_geocode(latlng)
     address = get_address_from_reverse_geocode(reverse_geocode_result)
 
-    nearby_result = gmaps.places_radar(latlng, 5000, keyword='address')
-    nearby_result = nearby_result['results']
+    # nearby_result = gmaps.places_radar(latlng, 1000, keyword='food')
+    # nearby_result = nearby_result['results']
+    # lats = []
+    # lngs = []
+    # for r in nearby_result:
+    #     pair = r['geometry']['location']['lat'], r['geometry']['location']['lng']
+    #     print 'Gmaps:\t', get_address_from_reverse_geocode(gmaps.reverse_geocode(pair))
+    #     print 'Uber:\t', (Uber.address_lookup(pair[0], pair[1]))['longAddress']
+    #     print
+    #     lats.append(pair[0])
+    #     lngs.append(pair[1])
+
+
+    hexgrid = generate_hexgrid(params.centroid, params.shells, params.major)
+    print len(hexgrid)
     lats = []
     lngs = []
-    for r in nearby_result:
-        pair = r['geometry']['location']['lat'], r['geometry']['location']['lng']
-        print 'Gmaps:\t', get_address_from_reverse_geocode(gmaps.reverse_geocode(pair))
-        print 'Uber:\t', (Uber.address_lookup(pair[0], pair[1]))['longAddress']
-        print
+    for pair in hexgrid:
         lats.append(pair[0])
         lngs.append(pair[1])
 
 
-
-
-    gmap = gmplot.GoogleMapPlotter(latlng[0], latlng[1], 14)
-
-    # gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
-    gmap.scatter(lats, lngs, 'k', marker=True)
-    # gmap.heatmap(heat_lats, heat_lngs)
-
-    gmap.draw("mymap.html")
+    drawMap()
