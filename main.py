3c0d6bbf41fb8fee5f8a3d65b384debbec7955d4
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-06-25 17:30:48 -0700

    added notes

--- main.py
+++ main.py
@@ -24,4 +24,19 @@ if __name__ == '__main__':
     Uber = UberWrapper()
     print Uber.address_lookup(47.575243199999996, -122.12976889999999)
 
-    
+    from googleplaces import GooglePlaces, types, lang
+
+    YOUR_API_KEY = 'AIzaSyAiFpFd85eMtfbvmVNEYuNds5TEF9FjIPI'
+
+    google_places = GooglePlaces(YOUR_API_KEY)
+
+    # You may prefer to use the text_search API, instead.
+    query_result = google_places.nearby_search(
+            location='London, England', keyword='Fish and Chips',
+            radius=20000, types=[types.TYPE_FOOD])
+    # If types param contains only 1 item the request to Google Places API
+    # will be send as type param to fullfil:
+    # http://googlegeodevelopers.blogspot.com.au/2016/02/changes-and-quality-improvements-in_16.html
+
+    if query_result.has_attributions:
+        print query_result.html_attributions
