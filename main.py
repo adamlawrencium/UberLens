adb5ceb86628455e6ee9ebc7a7aa7c5e040c6996
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-06-28 21:42:02 -0700

    color gradient in effect!

--- main.py
+++ main.py
@@ -35,9 +35,100 @@ def readAPI_key():
 def drawMap(pairs):
     gmap = gmplot.GoogleMapPlotter(params.centroid[0], params.centroid[1], 14)
     gmap.scatter(pairs['low'][1], pairs['low'][2], 'forestgreen', size=45, marker=False)
-    gmap.scatter(pairs['medium'][1], pairs['medium'][2], 'yellow', size=45, marker=False)
+    gmap.scatter(pairs['medium'][1], pairs['medium'][2], '#F4A460', size=45, marker=False)
     gmap.scatter(pairs['high'][1], pairs['high'][2], 'red', size=45, marker=False)
     gmap.draw("mymap.html")
+
+def drawMapWithGradient(fares_and_coordinates):
+    fares = []
+    for fare in fares_and_coordinates:
+        fares.append(fare[0])
+
+    num_distinct_prices = len(set(fares))
+    print 'distinct fares:',set(fares)
+    fares_and_coordinates.sort(key=lambda x: x[0])
+
+
+    # # # # # # # # # # # # # #
+    from colour import Color
+    red = Color("red")
+    colors = list(red.range_to(Color("green"), num_distinct_prices))
+    print num_distinct_prices, 'colors generated\n'
+
+
+    gmap = gmplot.GoogleMapPlotter(params.centroid[0], params.centroid[1], 14)
+    i = 0
+
+
+     # # # # # #  # # # #
+    fare_dic = {}
+    for fare in set(fares):
+        fare_dic[fare] = []
+
+    for fare in fares_and_coordinates:
+        fare_dic[fare[0]].append(fare)
+
+    gradient_dict = {}
+    for i in range(len(fare_dic)):
+        gradient_dict[colors[i].hex] = fare_dic[sorted(fare_dic)[i]]
+
+    print gradient_dict
+    for hex_fare in gradient_dict:
+        lats = [i[1] for i in gradient_dict[hex_fare]]
+        lngs = [i[2] for i in gradient_dict[hex_fare]]
+        gmap.scatter(lats, lngs, hex_fare, size=60, marker=False)
+
+    # # # # # # # # # # #
+    #
+    # while i < num_distinct_prices:
+    #     begin_color_band = fares_and_coordinates[i]
+    #     lats = []
+    #     lngs = []
+    #     j = i
+    #     print 'color of interest:', colors[i].hex
+    #     print 'starting fare:', fares_and_coordinates[i][0]
+    #     print
+    #     while j < len(fares_and_coordinates):
+    #         print 'checking if next fare (%f) is same as starting fare (%f)' % (fares_and_coordinates[j][0], fares_and_coordinates[i][0])
+    #         if fares_and_coordinates[j][0] == begin_color_band[0]:
+    #             print 'fares are the same!'
+    #             lats.append(fares_and_coordinates[j][1])
+    #             lngs.append(fares_and_coordinates[j][2])
+    #             print colors[i].hex
+    #             print 'lats',lats
+    #             print 'lngs',lngs
+    #             print
+    #         else:
+    #             print 'fares are not the same...'
+    #             break
+    #
+    #         j += 1
+    #
+    #     print '\nAdding points to map:'
+    #     print 'color',colors[i].hex
+    #     print 'lats',lats
+    #     print 'lngs',lngs
+    #     print
+    #     gmap.scatter(lats, lngs, colors[i].hex, size=45, marker=False)
+    #     print 'next color up!'
+    #     print
+    #
+    #         # lats.append(fares_and_coordinates[j][1])
+    #         # lngs.append(fares_and_coordinates[j][2])
+    #         # if fares_and_coordinates[j][0] != begin_color_band[0]:
+    #         #     print colors[i].hex
+    #         #     print 'lats',lats
+    #         #     print 'lngs',lngs
+    #         #     print
+    #         #     gmap.scatter(lats, lngs, colors[i].hex, size=45, marker=False)
+    #         #     break
+    #
+    #
+    #     i = j+1
+
+    gmap.draw("mymap.html")
+
+
 
 
 class UberWrapper(object):
@@ -102,6 +193,10 @@ class GMapsWrapper(object):
 
 if __name__ == '__main__':
 
+
+
+
+
     # POIs in Puget Sound
     msft_seattle = '320 Westlake Ave N, Seattle, WA 98109'
     msft_studioG = '3950 148th Ave NE, Redmond, WA 98052'
@@ -115,8 +210,8 @@ if __name__ == '__main__':
     # # # # # USER INPUTS # # # # #
     origin_of_travel = larkspur
     destination = msft_bellevue
-    num_shells = 10
-    major = 100
+    num_shells = 5
+    major = 200
     # # # # # # # # # # # # # # # #
 
 
@@ -161,30 +256,35 @@ if __name__ == '__main__':
 
         fare_range = [ float(x) for x in fare_string[1:].split('-') ]
         fare = sum(fare_range) / len(fare_range)
-        fares.append(fare)
+        fares.append([fare, centroid[0], centroid[1]])
 
-        print "[%d/%d]:\t$%d" % (counter, num_hexs, fare)
+        print "[%d/%d]:\t$%f" % (counter, num_hexs, fare)
         counter += 1
-        # time.sleep(0.25)
+        time.sleep(1)
 
+    print
     for i in range(len(fares)):
         if fares[i] == min(fares):# <=7: #
-            print 'low found', fares[i]
+            # print 'low found', fares[i]
             pairs['low'][0].append(fares[i])
             pairs['low'][1].append(hexgrid[i][0])
             pairs['low'][2].append(hexgrid[i][1])
         elif fares[i] == max(fares):#>= 11:
-            print 'high found', fares[i]
+            # print 'high found', fares[i]
             pairs['high'][0].append(fares[i])
             pairs['high'][1].append(hexgrid[i][0])
             pairs['high'][2].append(hexgrid[i][1])
         else:
-            print 'medium found', fares[i]
+            # print 'medium found', fares[i]
             pairs['medium'][0].append(fares[i])
             pairs['medium'][1].append(hexgrid[i][0])
             pairs['medium'][2].append(hexgrid[i][1])
 
-    drawMap(pairs)
+
+    # drawMap(pairs)
+    drawMapWithGradient(fares)
+
+
 
 
     # # # # # # DRAWING MAP # # # #
@@ -192,8 +292,8 @@ if __name__ == '__main__':
     import numpy as np
 
     x = fares
-    print x
+    # print x
     plt.hist(x, normed=True, bins=30)
     plt.ylabel('Probability');
 
-    plt.show()
+    # plt.show()
