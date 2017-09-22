82e1119c88830e7dd0d2691b989217d3c3e97324
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-09-22 00:38:26 -0400

    favicon to .ico

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'UberLens-logo-lens.png')));
+app.use(favicon(path.join(__dirname, 'public', 'UberLens-logo-lens.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
